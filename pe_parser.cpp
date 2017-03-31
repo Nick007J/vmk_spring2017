cc56dc859da054b596ce3e41ef4be3d13bbc75ab
Королев Николай Николаевич <nickvnuk@gmail.com>
2017-03-31 13:51:08 +0300

    Added parsing of 64-bit exes

--- pe_parser.cpp
+++ pe_parser.cpp
@@ -71,49 +71,78 @@ unsigned int ReadFileToBuffer( HANDLE fileHandle, char buffer[ BUFFER_SIZE ] )
   return returnValue;
 }
 
-void ParseFile( char* buffer, int bufferSize )
-{
 #define DEBUG_OUTPUT 0
-	IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)buffer;
-	if (dos_header->e_magic != IMAGE_DOS_SIGNATURE)
-		printf("Not a valid DOS header\n");
-	IMAGE_NT_HEADERS* nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
-	if (nt_header->Signature != IMAGE_NT_SIGNATURE)
-		printf("Not a valid NT signature\n");
-	IMAGE_FILE_HEADER* file_header = &nt_header->FileHeader;
-	WORD nsec = file_header->NumberOfSections;
-	IMAGE_OPTIONAL_HEADER* opt_header = &nt_header->OptionalHeader;
-	if (opt_header->Magic != 0x10B && opt_header->Magic != 0x107)
-		printf("Not a valid Win32 executable\n");
-	DWORD ib = opt_header->ImageBase;
-	DWORD ep = opt_header->AddressOfEntryPoint;
-	printf("Entry point (%X)\n", ep);
+
+int GetInfoFromNTHeader(void* p_optheader, ULONGLONG* ib, DWORD* ep)
+{
+    IMAGE_OPTIONAL_HEADER32* opth32 = (IMAGE_OPTIONAL_HEADER32*)p_optheader;
+    if (opth32->Magic == 0x10B || opth32->Magic == 0x107) {
 #if DEBUG_OUTPUT
-	printf("Image base (%X)\n", ib);
+        printf("32 bit\n");
 #endif
-	IMAGE_SECTION_HEADER* section = IMAGE_FIRST_SECTION(nt_header);
-	for (WORD i = 0; i < nsec; i++, section++) {
-		DWORD va = section->VirtualAddress;
-		DWORD size = section->Misc.VirtualSize;
+        *ib = opth32->ImageBase;
+        *ep = opth32->AddressOfEntryPoint;
+        return 1;
+    }
+    IMAGE_OPTIONAL_HEADER64* opth64 = (IMAGE_OPTIONAL_HEADER64*)p_optheader;
+    if (opth64->Magic == 0x20B) {
 #if DEBUG_OUTPUT
-		char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
-		memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
-		name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
-		printf("Name: %s, Start: %X, Size: %X, End: %X\n", name, va, size, va+size);
-		free(name);
+        printf("64 bit\n");
 #endif
-		if (va < ep && ep < va + size) {
-			char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
-			memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
-			name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
-			printf("In section %d, %s\n", i, name);
-			DWORD offset = (ep - va) * 100 / size;
-			printf("Offset in section %X, %d%%\n", ep - va, offset);
-			free(name);
-			return;
-		}
-	}
-	printf("Section of entry point not found\n");
+        *ib = opth64->ImageBase;
+        *ep = opth64->AddressOfEntryPoint;
+        return 1;
+    }
+    return 0;
+}
+
+void ParseFile(char* buffer, int bufferSize)
+{
+    IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)buffer;
+    if (dos_header->e_magic != IMAGE_DOS_SIGNATURE) {
+        printf("Not a valid DOS header\n");
+        return;
+    }
+    IMAGE_NT_HEADERS* nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
+    if (nt_header->Signature != IMAGE_NT_SIGNATURE) {
+        printf("Not a valid NT signature\n");
+        return;
+    }
+    IMAGE_FILE_HEADER* file_header = &nt_header->FileHeader;
+    WORD nsec = file_header->NumberOfSections;
+    ULONGLONG ib;
+    DWORD ep;
+    if (!GetInfoFromNTHeader(&nt_header->OptionalHeader, &ib, &ep)) {
+        printf("Not a valid magic in optional header\n");
+        return;
+    }
+    printf("Entry point (%X)\n", ep);
+#if DEBUG_OUTPUT
+    printf("Image base (%X)\n", ib);
+#endif
+    IMAGE_SECTION_HEADER* section = IMAGE_FIRST_SECTION(nt_header);
+    for (WORD i = 0; i < nsec; i++, section++) {
+        DWORD va = section->VirtualAddress;
+        DWORD size = section->Misc.VirtualSize;
+#if DEBUG_OUTPUT
+        char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
+        memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
+        name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
+        printf("Name: %s, Start: %X, Size: %X, End: %X\n", name, va, size, va+size);
+        free(name);
+#endif
+        if (va < ep && ep < va + size) {
+            char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
+            memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
+            name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
+            printf("In section %d, %s\n", i, name);
+            DWORD offset = (ep - va) * 100 / size;
+            printf("Offset in section %X, %d%%\n", ep - va, offset);
+            free(name);
+            return;
+        }
+    }
+    printf("Section of entry point not found\n");
     printf("Buffer length: %d\n", bufferSize);
 }
 
