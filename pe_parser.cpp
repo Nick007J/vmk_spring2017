72b8fdab0fbc9d8074511c94a2d70c31b4c45398
Королев Николай Николаевич <nickvnuk@gmail.com>
2017-04-04 18:03:58 +0300

    Style fix
    С пунктом 1 соглашусь. По поводу 2, 3 и 4. Прямо в указанной статье о goto указаны случаи допустимого использования этого оператора. Большинство руководств по стилям программирования указывают те же два случая, что и википедия. Его можно было бы избежать, если отказаться от return'ов, но тогда внешний вид программы пострадает из-за ненужного нагромождения вложенных if'ов либо множества булевых переменных. Ну и мне лично нравится больше именно вариант с goto (но надо заметить, что это за счет того, что я пишу в стиле чистого Си, где нет обработки исключений и механизма деструкторов). Проблема 2 соответственно решилась за счет заменой return'ов на goto. По поводу пункта 5 - вполне допускаю и даже почти не сомневаюсь, что я где-нибудь ошибся, так что жду :)

--- pe_parser.cpp
+++ pe_parser.cpp
@@ -95,29 +95,47 @@ int GetInfoFromNTHeader(void* opt_header, ULONGLONG* image_base, DWORD* entry_po
     return 0;
 }
 
+bool ReadFileAgain(unsigned int required_size, unsigned int* new_size, char** buffer, bool* memory_allocated, HANDLE* file_handle)
+{
+    unsigned int pure_size = (required_size / BUFFER_SIZE + 1) * BUFFER_SIZE;
+    if (*memory_allocated)
+        free(*buffer);
+    *buffer = (char*)malloc(pure_size);
+    *memory_allocated = true;
+    *new_size = ReadFileToBuffer(*file_handle, *buffer, pure_size);
+    if (*new_size < required_size)
+        return false;
+    return true;
+}
+
+char* GetSectionName(IMAGE_SECTION_HEADER* section)
+{
+    char* new_name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
+    memcpy(new_name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
+    new_name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
+    return new_name;
+}
+
 void ParseFile(char* buffer, unsigned int buffer_size, HANDLE* file_handle)
 {
     bool memory_allocated = false;
     IMAGE_DOS_HEADER* dos_header = (IMAGE_DOS_HEADER*)buffer;
     if (IMAGE_DOS_SIGNATURE != dos_header->e_magic) {
         printf("Not a valid DOS header\n");
-        return;
+        goto cleanup;
     }
     IMAGE_NT_HEADERS* nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
     if ((unsigned int)nt_header - (unsigned int)buffer + sizeof(IMAGE_NT_HEADERS) > buffer_size) {
-        unsigned int required_size = ((unsigned int)nt_header - (unsigned int)buffer + sizeof(IMAGE_NT_HEADERS) / BUFFER_SIZE) * (BUFFER_SIZE + 1);
-        buffer = (char*)malloc(required_size);
-        memory_allocated = true;
-        buffer_size = ReadFileToBuffer(*file_handle, buffer, required_size);
-        nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
-        if (buffer_size < (unsigned int)nt_header - (unsigned int)buffer + sizeof(IMAGE_NT_HEADERS)) {
+        if (!ReadFileAgain((unsigned int)nt_header - (unsigned int)buffer + sizeof(IMAGE_NT_HEADERS), &buffer_size, &buffer, &memory_allocated, file_handle)) {
             printf("File doesn't contain correct NT header\n");
-            return;
+            goto cleanup;
         }
+        dos_header = (IMAGE_DOS_HEADER*)buffer;
+        nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
     }
     if (IMAGE_NT_SIGNATURE != nt_header->Signature) {
         printf("Not a valid NT signature\n");
-        return;
+        goto cleanup;
     }
     IMAGE_FILE_HEADER* file_header = &nt_header->FileHeader;
     WORD nsec = file_header->NumberOfSections;
@@ -125,34 +143,25 @@ void ParseFile(char* buffer, unsigned int buffer_size, HANDLE* file_handle)
     DWORD entry_point;
     if (!GetInfoFromNTHeader(&nt_header->OptionalHeader, &image_base, &entry_point)) {
         printf("Not a valid magic in optional header\n");
-        return;
+        goto cleanup;
     }
     printf("Entry point (%llX)\n", entry_point + image_base);
     IMAGE_SECTION_HEADER* section = IMAGE_FIRST_SECTION(nt_header);
     if ((unsigned int)section - (unsigned int)buffer + sizeof(IMAGE_SECTION_HEADER)*nsec > buffer_size) {
-        unsigned int required_size = ((unsigned int)section - (unsigned int)buffer + sizeof(IMAGE_SECTION_HEADER)*nsec / BUFFER_SIZE) * (BUFFER_SIZE + 1);
-        if (memory_allocated)
-            free(buffer);
-        buffer = (char*)malloc(required_size);
-        buffer_size = ReadFileToBuffer(*file_handle, buffer, required_size);
-        section = IMAGE_FIRST_SECTION((IMAGE_NT_HEADERS*)(buffer + (((IMAGE_DOS_HEADER*)buffer)->e_lfanew)));
-        if (buffer_size < (int)section - (int)buffer + sizeof(IMAGE_SECTION_HEADER)*nsec) {
+
+        if (!ReadFileAgain(((unsigned int)section - (unsigned int)buffer + sizeof(IMAGE_SECTION_HEADER)*nsec), &buffer_size, &buffer, &memory_allocated, file_handle)) {
             printf("Header doesn't contain all sections\n");
             return;
         }
+        dos_header = (IMAGE_DOS_HEADER*)buffer;
+        nt_header = (IMAGE_NT_HEADERS*)(buffer + dos_header->e_lfanew);
+        section = IMAGE_FIRST_SECTION(nt_header);
     }
     for (WORD i = 0; i < nsec; i++, section++) {
         DWORD virt_address = section->VirtualAddress;
         DWORD size = section->Misc.VirtualSize;
-        char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
-        memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
-        name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
-        printf("Name: %s, Start: %X, Size: %X, End: %X\n", name, virt_address, size, virt_address + size);
-        free(name);
         if (virt_address <= entry_point && entry_point < virt_address + size) {
-            char* name = (char*)malloc(IMAGE_SIZEOF_SHORT_NAME + 1);
-            memcpy(name, section->Name, IMAGE_SIZEOF_SHORT_NAME);
-            name[IMAGE_SIZEOF_SHORT_NAME] = '\0';
+            char* name = GetSectionName(section);
             printf("In section %d, %s\n", i, name);
             DWORD offset = (entry_point - virt_address) * 100 / size;
             printf("Offset in section %lX, %ld %%\n", entry_point - virt_address, offset);
